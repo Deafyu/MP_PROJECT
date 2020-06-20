0f41248ed09575207f4289a2ba7a33201146a430
deafyu <quuba68@wp.pl>
2020-06-20 13:22:04 +0200

    start

---
+++ cmake-build-debug/CMakeFiles/MP_PROJECT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MP_PROJECT.dir/main.cpp.obj"
+  "MP_PROJECT.exe"
+  "MP_PROJECT.exe.manifest"
+  "MP_PROJECT.pdb"
+  "libMP_PROJECT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MP_PROJECT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
